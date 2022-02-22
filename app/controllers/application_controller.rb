cf0eb3394820c890bcd97fce62644f8598f7b4a8
Eisa Alserkal <eisa.alserkal@gmail.com>
2022-02-22 12:06:23 +0400

    Added the pundit configuration, and set up the methods of spaces with a listing method for user specific listings, index and show can be accessed without being signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -7,5 +8,24 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: :listings, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :listings, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  # def user_not_authorized
+  #   flash[:alert] = "You are not authorized to perform this action."
+  #   redirect_to(root_path)
+  # end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/  || params[:controller] =~ /(^(rails_)?spaces)|(^pages$)/
   end
 end
--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,34 +1,46 @@
 class SpacesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @spaces = Space.all
   end
 
+  def listings
+    @spaces = policy_scope(Space).order(created_at: :desc)
+  end
+
   def show
     @space = Space.find(params[:id])
+    authorize @space
   end
 
   def new
     @space = Space.new
+    authorize @space
   end
 
   def create
     @space = Space.new(space_params)
+    @space.user = current_user
+    authorize @space
     @space.save
     redirect_to space_path(@space)
   end
 
   def edit
     @space = Space.find(params[:id])
+    authorize @space
   end
 
   def update
     @space = Space.find(params[:id])
+    authorize @space
     @space.update(space_params)
     redirect_to space_path(@space)
   end
 
   def destroy
     @space = Space.find(params[:id])
+    authorize @space
     @space.destroy
 
     redirect_to spaces_path
@@ -37,6 +49,6 @@ class SpacesController < ApplicationController
   private
 
   def space_params
-    params.require(:space).permit(:name, :location, :hourly_rate, :user_id)
+    params.require(:space).permit(:name, :location, :hourly_rate)
   end
 end
