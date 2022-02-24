2c95bd0a0bcbdd9088459c941ae937d5efca8253
Ahmed Albaiti <ahmedalbaiti1@gmail.com>
2022-02-24 18:29:52 +0400

    added games and overview to space

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_102852) do
+ActiveRecord::Schema.define(version: 2022_02_24_142905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,8 @@ ActiveRecord::Schema.define(version: 2022_02_24_102852) do
     t.string "vr_headset"
     t.float "latitude"
     t.float "longitude"
+    t.string "games"
+    t.string "overview"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
