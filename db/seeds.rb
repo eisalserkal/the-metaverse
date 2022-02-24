8a032c9e9e90a086f280d7843613573a986a2b70
Ahmed Albaiti <ahmedalbaiti1@gmail.com>
2022-02-24 16:10:58 +0400

    completed mapbox on index

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ puts "Creating Users & 5 Spaces each..."
 5.times do
   user = User.create(email:Faker::Internet.email, password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
   5.times do
-    Space.create(name: Faker::Address.city, location: Faker::Address.street_address, hourly_rate: rand(300..350), user: user )
+    Space.create(name: Faker::Address.city, location: Faker::Address.street_address, hourly_rate: rand(300..350), user: user, vr_headset: ['Oculus Rift', 'Oculus Quest', 'HTC Vive', 'Playstation VR', 'Valve Index'].sample )
   end
 
 end
