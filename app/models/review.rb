9399fc1a01db1ed1a51c6bad3309a9e729ae1290
Eisa Alserkal <eisa.alserkal@gmail.com>
2022-02-25 12:47:25 +0400

    some front-end and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :booking
   has_one :user, through: :booking, dependent: :destroy
   has_one :space, through: :booking, dependent: :destroy
+  validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
